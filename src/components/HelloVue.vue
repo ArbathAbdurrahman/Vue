69fe527506c7f5b8ec4d01b45e93f5dfad874021
Arbath Abdurrahman <arbath21@gmail.com>
2025-02-28 14:39:23 +0700

    Template dan Directive

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -1,9 +1,34 @@
 <script setup>
 console.info("Hello Vue");
+
+const hello = "<b>Hello dari Vue</b>"
+const classHello = "hello"
+const id = "hello";
+const buttonOff = true;
+const data = {
+    id: "hello",
+    class: "hello"
+};
+
+function halo(){
+    return "Fungsi Hello!";
+}
 </script>
 
 <template>
     <h1 class="hello">Hello Vue</h1>
+    <h1>{{ hello.toUpperCase() }}</h1>
+    <h1>{{ 80 * 80 }}</h1>
+    <h1>{{ halo() }}</h1>
+    <h1 class="hello">{{ hello }}</h1>
+    <div v-html="hello"></div>
+    <!-- v-bind -->
+    <div :id :class="classHello" v-html="hello"></div>
+    <!-- render objek data directive bindding-->
+    <div v-bind="data" v-html="hello"></div>
+    <div :class="data.class" v-html="hello"></div>
+    <!-- boolean -->
+    <button :disabled="buttonOff">Ini Button</button>
 </template>
 
 <style>
