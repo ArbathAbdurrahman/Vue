131629d48c8dd47e4cc2575c6587efe5a9ec2c28
Arbath Abdurrahman <arbath21@gmail.com>
2025-02-28 13:35:58 +0700

    Hello World!

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+
+  // tambahkan untuk multiple html
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        hello: "hello.html",
+      }
+    }
+  }
+})
